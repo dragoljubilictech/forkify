84908b379e91da43169dad05b116a34b4d7c6619
dragoljubilictech@gmail.com <dragoljubilictech@gmail.com>
2021-12-02 10:46:00 +0100

    Refactoring

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,6 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +14,25 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpiner();
+
+    //Loading recipe
+    await model.loadRecipe(id);
+
+    //Rendering recipe
+
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
